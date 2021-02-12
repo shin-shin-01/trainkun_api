824a01b9ef490e3169c73047ea06733295d63ad9
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-12 11:04:36 +0900

    rename -> wishapp-api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "trainkun_api_production"
+  # config.active_job.queue_name_prefix = "wishapp_api_production"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
