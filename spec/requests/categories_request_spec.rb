60d216ccf61691e8b42aa8cbce4f30cf7a1c13c8
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-16 12:29:48 +0900

    bin/bundle exec rubocop -A

--- spec/requests/categories_request_spec.rb
+++ spec/requests/categories_request_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
   describe 'POST /categories' do
-    let(:headers) { { ACCEPT: 'application/json'} }
+    let(:headers) { { ACCEPT: 'application/json' } }
     let(:endpoint) { '/api/v1/categories' }
-    let(:request) { post endpoint, headers: headers, as: :json, params: { category: category_params }}
-    
-    let(:category_params) { { name: name }}
+    let(:request) { post endpoint, headers: headers, as: :json, params: { category: category_params } }
+
+    let(:category_params) { { name: name } }
     let(:name) { Faker::Alphanumeric.alpha(number: 10) }
 
     context 'SUCCESS' do
@@ -19,11 +21,12 @@ RSpec.describe 'Categories', type: :request do
     context 'SUCCESS on save' do
       it 'increase total number of category by 1' do
         expect { request }.to change { Category.count }.by(1)
-      end 
+      end
     end
 
     context 'ERROR: name not found' do
       let(:name) { nil }
+
       before { request }
 
       it_behaves_like 'API returns json'
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RequestHelpers
   module JsonHelpers
     def json
