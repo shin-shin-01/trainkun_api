5fa6586d36abe3f110d4b2511b435c1e24386346
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-16 12:09:42 +0900

    imple: create

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -5,6 +5,19 @@ module Api
     class CategoriesController < ApplicationController
       # POST /categories
       def create
+        new_category = Category.new(category_params)
+
+        if new_category.save
+          render json: { data: new_category }, status: :created
+        else
+          render json: { error: 'Failed to create new Category', data: new_category.errors }, status: :bad_request
+        end
+      end
+
+      private
+
+      def category_params
+        params.require(:category).permit(:name)
       end
     end
   end
