d8e408ca02b1cbfaaccdbaeee78a0e55f026b97f
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-20 00:54:18 +0900

    fix: nil into string error

--- app/services/authorization_service.rb
+++ app/services/authorization_service.rb
@@ -2,8 +2,13 @@
 
 class AuthorizationService
   def initialize(headers = {})
-    @request_url = ENV['AUTHORIZATION_URL'] + headers['LINE_Authorization']
+    @request_url = ENV['AUTHORIZATION_URL'] + extract_access_token(headers)
     @client_id = String(ENV['LINE_CLIENT_ID'])
+  end
+
+  def extract_access_token(headers)
+    return headers['LINE_Authorization'] if headers['LINE_Authorization'].present?
+    return ""
   end
 
   def verify_token
