c6dfb3a50ed630044562f3466318f2672198f476
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-20 22:30:15 +0900

    update: request in requestExamples

--- spec/support/shared_examples/request_examples.rb
+++ spec/support/shared_examples/request_examples.rb
@@ -1,25 +1,43 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'API returns json' do
-  specify { expect(response.content_type).to eq('application/json; charset=utf-8') }
+  specify do
+    request
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+  end
 end
 
 RSpec.shared_examples 'response status code: OK' do
-  specify { expect(response).to have_http_status(:ok) }
+  specify do
+    request
+    expect(response).to have_http_status(:ok)
+  end
 end
 
 RSpec.shared_examples 'response status code: CREATED' do
-  specify { expect(response).to have_http_status(:created) }
+  specify do
+    request
+    expect(response).to have_http_status(:created)
+  end
 end
 
 RSpec.shared_examples 'response status code: BAD REQUEST' do
-  specify { expect(response).to have_http_status(:bad_request) }
+  specify do
+    request
+    expect(response).to have_http_status(:bad_request)
+  end
 end
 
 RSpec.shared_examples 'response status code: NOT FOUND' do
-  specify { expect(response).to have_http_status(:not_found) }
+  specify do
+    request
+    expect(response).to have_http_status(:not_found)
+  end
 end
 
 RSpec.shared_examples 'response status code: INTERNAL SERVER ERROR' do
-  specify { expect(response).to have_http_status(:internal_server_error) }
+  specify do
+    request
+    expect(response).to have_http_status(:internal_server_error)
+  end
 end
