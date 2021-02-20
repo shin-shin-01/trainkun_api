c34a139b549b54d102bc54c8e35b325a43960e56
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-20 22:30:41 +0900

    test: endpoint #create /users

---
+++ spec/requests/users_request_spec.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  before { skip_token_authorization }
+
+  describe 'POST /users' do
+    let(:request) { post '/api/v1/users', headers: { ACCEPT: 'application/json' }, as: :json, params: { user: user_params } }
+
+    let(:user_params) do
+      {
+        name: name,
+        uid: uid
+      }
+    end
+
+    let(:name) { Faker::Alphanumeric.alpha(number: 10) }
+    let(:uid) { Faker::Alphanumeric.alpha(number: 10) }
+
+    context 'SUCCESS: createUser' do
+      it_behaves_like 'API returns json'
+      it_behaves_like 'response status code: CREATED'
+      it 'increase total number of user 1' do
+        expect { request }.to change { User.count }.by(1)
+      end
+
+      it 'returns: created user' do
+        request
+        expect(json['user']).to include(
+          {
+            'name' => name,
+            'uid' => uid
+          }
+        )
+      end
+    end
+
+    context 'SUCCESS: already created user' do
+      let!(:user) { create(:user, name: name, uid: uid) }
+
+      it_behaves_like 'API returns json'
+      it_behaves_like 'response status code: OK'
+      it 'NOT: increase total number of user' do
+        expect { request }.to change { User.count }.by(0)
+      end
+
+      it 'returns: created user' do
+        request
+        expect(json['user']).to include(
+          {
+            'name' => user.name,
+            'uid' => user.uid
+          }
+        )
+      end
+    end
+
+    context 'ERROR: not name in params' do
+      let(:name) { nil }
+
+      it_behaves_like 'API returns json'
+      it_behaves_like 'response status code: BAD REQUEST'
+    end
+
+    context 'ERROR: not uid in params' do
+      let(:uid) { nil }
+
+      it_behaves_like 'API returns json'
+      it_behaves_like 'response status code: BAD REQUEST'
+    end
+  end
+end
