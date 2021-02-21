c6e6a0593fd9cb47383aa0c3dc3e088ebc7d8c02
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-21 12:12:46 +0900

    associate with wishes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :wishes, dependent: :restrict_with_error
+
   validates :name, length: { maximum: 255 }, presence: true
   validates :uid, length: { maximum: 255 }, presence: true, uniqueness: { case_sensitive: true }
 
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:wishes) }
+  end
+
   describe 'validation: name' do
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
     it { is_expected.to validate_presence_of(:name) }
