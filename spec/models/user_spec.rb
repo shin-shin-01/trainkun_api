f8a5f3eb6ec995dd7f3f9d11966b2a13da4638b9
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-21 12:07:21 +0900

    rm skip_authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   describe 'validation: name' do
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
     it { is_expected.to validate_presence_of(:name) }
