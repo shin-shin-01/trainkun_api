f75acbeda38786d071adb7e8514c04b3a50d207b
Kaze Shindo <kaze.bknb@outlook.jp>
2021-02-27 17:08:11 +0900

    fix 'users' to :users

--- db/migrate/20210225160018_create_friends.rb
+++ db/migrate/20210225160018_create_friends.rb
@@ -2,8 +2,8 @@ class CreateFriends < ActiveRecord::Migration[6.0]
   def change
     create_table :friends do |t|
       # TODO: 友達の仕組みをアップデートする
-      t.references :user, null: false, foreign_key: { to_table: 'users' }
-      t.references :friend_user, null: false, foreign_key: { to_table: 'users' }
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :friend_user, null: false, foreign_key: { to_table: :users }
       t.boolean :deleted, null: false, default: false
 
       t.timestamps
