2850fce02421a3f4cdc93ce6ae58637f0cc8b2bc
Kaze Shindo <kaze.bknb@outlook.jp>
2021-03-17 09:47:33 +0900

    create image table

---
+++ db/migrate/20210317160018_create_images.rb
@@ -0,0 +1,10 @@
+class CreateImages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :images do |t|
+      t.references :wish, null: false, foreign_key: true
+      t.string :url, null: false
+
+      t.timestamps
+    end
+  end
+end
